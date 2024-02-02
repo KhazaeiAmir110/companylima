605eb99264c5152a7c6fdf9086bd1c3956dcda38
test_python <khazaei.amir110@gmail.com>
2024-02-02 19:12:05 +0330

    Aparat, because it is written with JavaScript, its videos cannot be downloaded.

--- bahman/webscraping/video/aparat.py
+++ bahman/webscraping/video/aparat.py
@@ -3,6 +3,10 @@ import re
 import random
 from bs4 import BeautifulSoup
 
+
+"""
+Aparat, because it is written with JavaScript, its videos cannot be downloaded.
+"""
 def download(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -17,4 +21,18 @@ def download(url):
                     file.write(video)
 
 
-download('https://www.aparat.com/v/Ev3mp')
+# download('https://www.aparat.com/v/Ev3mp')
+
+def aparat(url, quality=None):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    body = soup.find('body')
+    xq = body.find('div', class_='episode_container')
+    div = body.find_all('div', attrs={'class': 'app'})
+    li = div[0].find_all('li')
+    for di in li:
+        print(di)
+
+    print(body)
+
+aparat('https://www.aparat.com/v/Ev3mp')
